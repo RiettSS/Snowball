40b8a8f6fc7c60dc26f170d8cf38f9a088e79992
Artem Demchenko <rietttube@gmail.com>
2022-11-19 16:21:42 +0300

    LevelStart analytics event is fixed

--- Assets/Scripts/SceneLoading/SceneLoaderView.cs
+++ Assets/Scripts/SceneLoading/SceneLoaderView.cs
@@ -1,12 +1,20 @@
 using Firebase.Analytics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Zenject;
 
 namespace SceneLoading
 {
     public class SceneLoaderView : MonoBehaviour
     {
         [SerializeField] private GameObject LoadingScreen;
+        private LevelLoader _levelLoader;
+        
+        [Inject]
+        public void Construct(LevelLoader levelLoader)
+        {
+            _levelLoader = levelLoader;
+        }
         
         public void OnLoadingStarted()
         {
@@ -16,8 +24,17 @@ namespace SceneLoading
         public void OnLoaded()
         {
             LoadingScreen.SetActive(false);
-            FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLevelStart,
-                new Parameter(FirebaseAnalytics.ParameterLevelName, SceneManager.GetActiveScene().name));
+            var sceneName = SceneManager.GetActiveScene().name;
+            if (sceneName != "LevelScreen")
+            {
+                FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLevelStart,
+                    new Parameter(FirebaseAnalytics.ParameterLevelName, SceneManager.GetActiveScene().name));
+            }
+            else
+            {
+                FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLevelStart,
+                    new Parameter(FirebaseAnalytics.ParameterLevelName, _levelLoader.CurrentLevel));
+            }
         }
     }
 }
