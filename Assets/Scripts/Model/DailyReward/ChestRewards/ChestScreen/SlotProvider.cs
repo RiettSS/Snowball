89be100458b60e474b6486a97de8e258f3afc307
Artem Demchenko <rietttube@gmail.com>
2022-11-19 06:31:26 +0300

    Prize Machine Random Generation Implemented

--- Assets/Scripts/Model/DailyReward/ChestRewards/ChestScreen/SlotProvider.cs
+++ Assets/Scripts/Model/DailyReward/ChestRewards/ChestScreen/SlotProvider.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Model.DailyReward.ChestRewards.ChestScreen
 {
@@ -10,22 +13,71 @@ namespace Model.DailyReward.ChestRewards.ChestScreen
         {
             _slotFactory = slotFactory;
         }
-        
+
         public List<Slot> GetSlots()
         {
+            var creationCommands = new List<CreateSlotCommand>();
+
+            creationCommands.Add(new CreateCoinsSlotCommand(_slotFactory, 50));
+            creationCommands.Add(new CreateCoinsSlotCommand(_slotFactory, 50));
+            creationCommands.Add(new CreateCoinsSlotCommand(_slotFactory, 50));
+            creationCommands.Add(new CreateCoinsSlotCommand(_slotFactory, 500));
+            creationCommands.Add(new CreateCoinsSlotCommand(_slotFactory, 500));
+            creationCommands.Add(new CreateCoinsSlotCommand(_slotFactory, 500));
+            creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
+            creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
+            creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
+            creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
+
+            var nums = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            nums.Shuffle();
+
             var slots = new List<Slot>();
             
-            slots.Add(_slotFactory.CreateCoinsSlot(50));
-            slots.Add(_slotFactory.CreateCoinsSlot(50));
-            slots.Add(_slotFactory.CreateCoinsSlot(50));
-            slots.Add(_slotFactory.CreateCrystalsSlot(5));
-            slots.Add(_slotFactory.CreateCrystalsSlot(5));
-            slots.Add(_slotFactory.CreateCrystalsSlot(5));
-            slots.Add(_slotFactory.CreateCoinsSlot(400));
-            slots.Add(_slotFactory.CreateCoinsSlot(400));
-            slots.Add(_slotFactory.CreateCoinsSlot(400));
+            foreach (var num in nums)
+            {
+                slots.Add(creationCommands[num].CreateSlot());
+            }
 
             return slots;
+        }
+    }
+
+    public abstract class CreateSlotCommand
+    {
+        protected ChestSlotFactory _factory;
+        protected int _amount;
+
+        protected CreateSlotCommand(ChestSlotFactory factory, int amount)
+        {
+            _factory = factory;
+            _amount = amount;
+        }
+
+        public abstract Slot CreateSlot();
+    }
+
+    public class CreateCoinsSlotCommand : CreateSlotCommand
+    {
+        public CreateCoinsSlotCommand(ChestSlotFactory factory, int amount) : base(factory, amount)
+        {
+        }
+
+        public override Slot CreateSlot()
+        {
+            return _factory.CreateCoinsSlot(_amount);
+        }
+    }
+    
+    public class CreateCrystalsSlotCommand : CreateSlotCommand
+    {
+        public CreateCrystalsSlotCommand(ChestSlotFactory factory, int amount) : base(factory, amount)
+        {
+        }
+
+        public override Slot CreateSlot()
+        {
+            return _factory.CreateCrystalsSlot(_amount);
         }
     }
 }
