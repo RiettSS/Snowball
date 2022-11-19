1528f99317468b5eb1c71adba6ef22b299dde39c
Artem Demchenko <rietttube@gmail.com>
2022-11-19 06:32:05 +0300

    Minor Fixes

--- Assets/Scripts/Model/DailyReward/ChestRewards/ChestScreen/SlotProvider.cs
+++ Assets/Scripts/Model/DailyReward/ChestRewards/ChestScreen/SlotProvider.cs
@@ -27,7 +27,6 @@ namespace Model.DailyReward.ChestRewards.ChestScreen
             creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
             creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
             creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
-            creationCommands.Add(new CreateCrystalsSlotCommand(_slotFactory, 10));
 
             var nums = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
             nums.Shuffle();
